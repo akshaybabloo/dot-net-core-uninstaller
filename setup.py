c09ca5c22623c1125ddfa037060bd394aef5d312
Akshay Raj Gollahalli <akshaybabloo@gmail.com>
2019-09-08 13:50:38 +1200

    dropped support for python 3.5

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: POSIX',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Intended Audience :: Developers',
